53d9abbd7390fed4310c1f2eda1ab26e7867bd93
Alex30340 <alex.navarro30340@gmail.com>
2025-03-27 21:59:06 +0100

    Update app_instance.py

--- core/app_instance.py
+++ core/app_instance.py
@@ -1,5 +1,11 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, use_pages=False, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(
+    __name__,
+    use_pages=True,  # ← Important pour que les pages fonctionnent
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True  # ← Utile pour les callbacks définis dans les pages
+)
+
 server = app.server
